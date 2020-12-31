9df240084bc00fa84c92c1c5d5d88b6255d70d9f
ericfleishman <ericfleishmanwork@gmail.com>
2020-12-31 13:34:31 -0600

    Version 1.00 -- Main file changed around to run all day and detect the time the market is open.

--- main.py
+++ main.py
@@ -6,30 +6,28 @@ import sellLib
 import timeControl as tc
 from classes import *
 
-#Open file containing tickers
 tickers = pd.read_csv('data.csv')
 df = pd.DataFrame(tickers)
 
-#Create Stock Object
 stocks = []
-for i in range(55):
+for i in range(5):
     ticker = df.loc[i, "Ticker"].strip()
     stocks.append(Stock(ticker))
 
-market_open = tc.market_status()
-
-#If market is open get all of the attributes for the stocks
-if market_open is True:
-    attributesLib.get_attributes(stocks)
-
-while market_open is True:
-    #Make list of stocks to buy
-    buy_list = chooseStockLib.generate_buy_list(stocks)
-
-    #Buy stocks
-    buyLib.buy_stocks(buy_list)
-
-    #Sell stocks
-    sellLib.sell_stocks(stocks)
-
+while True:
     market_open = tc.market_status()
+
+    if market_open is True:
+        attributesLib.get_attributes(stocks)
+
+    while market_open is True:
+        buy_list = chooseStockLib.generate_buy_list(stocks)
+
+        buyLib.buy_stocks(buy_list)
+
+        sellLib.sell_stocks()
+
+        market_open = tc.market_status()
+
+
+
