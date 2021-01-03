59c2bcf6b705c2469ca5623600ab87a9f4611af1
ericfleishman <ericfleishmanwork@gmail.com>
2021-01-03 15:57:57 -0600

    Version 1.01 -- Check Notes

--- main.py
+++ main.py
@@ -10,22 +10,24 @@ tickers = pd.read_csv('data.csv')
 df = pd.DataFrame(tickers)
 
 stocks = []
-for i in range(5):
+stored_stocks = []
+for i in range(len(df)):
     ticker = df.loc[i, "Ticker"].strip()
     stocks.append(Stock(ticker))
 
 while True:
     market_open = tc.market_status()
 
-    if market_open is True:
+    if market_open is True and len(stored_stocks) < 1:
         attributesLib.get_attributes(stocks)
+        stored_stocks = stocks
 
     while market_open is True:
         buy_list = chooseStockLib.generate_buy_list(stocks)
 
         buyLib.buy_stocks(buy_list)
 
-        sellLib.sell_stocks()
+        sellLib.sell_stocks(stored_stocks)
 
         market_open = tc.market_status()
 
--- sellLib.py
+++ sellLib.py
@@ -4,37 +4,28 @@ import pandas as pd
 from classes import *
 
 #Sells stocks that are determined to be sold
-def sell_stocks():
+def sell_stocks(stored):
+    owned_tickers = get_api_tickers()
     api = buyLib.get_api()
-    positions = api.list_positions()
-    df = pd.DataFrame(positions)
-    stocks = get_stock_objects(df)
-    attributesLib.get_attributes(stocks)
 
-    for i in stocks:
-        sell = determine_if_sell(i)
-        if sell is True:
+    for i in owned_tickers:
+        for j in stored:
+            if i == j.ticker:
+                i = j
+    for i in owned_tickers:
+        if i.moving50 < i.moving200:
             print("Selling: ", i.ticker)
             api.submit_order(symbol=str(i.ticker), qty="10", side="sell", type="market", time_in_force="day")
         else:
             print("Not Selling: ", i.ticker)
 
-#Takes the tickers returned by the Alpaca API and makes them into Stock Objects
-def get_stock_objects(df):
-    objects = []
+def get_api_tickers():
+    tickers = []
+    api = buyLib.get_api()
+    positions = api.list_positions()
+    df = pd.DataFrame(positions)
+
     for i in range(len(df)):
-        ticker = df.loc[i,0]
-        objects.append(Stock(ticker.symbol))
-    return objects
+        tickers.append(Stock(df.loc[i,0].symbol))
 
-#Determines if a stock is to be sold based on 50 and 200 day moving averages
-def determine_if_sell(stock):
-    moving50 = stock.moving50
-    moving200 = stock.moving200
-    if moving50 < moving200:
-        return True
-    else:
-        return False
-
-#PLAN FOR REVISION: Needs to take the stocks already owned from the API and get their attributes from the attribute list already stored. Doing this would speed up program's
-#Execution time
+    return tickers
